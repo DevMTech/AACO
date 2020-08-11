3d0081ffdc08aa6ba9b68e55338b74e414218e18
debajyotim <debajyotim@cdac.in>
2020-08-11 12:17:25 +0530

    AACO OTA HTTP POST
    AACO OTA HTTP POST template added

---
+++ AACO_OTA_DHPOST_v8/SSID.h
@@ -0,0 +1,11 @@
+#ifndef SSID_H_
+#define SSID_H_
+
+//const char* ssid = "CDACIoT";       const char* password = "hcdc_IoT";
+//const char* ssid = "Xperia";        const char* password = "Hello1214";
+//const char* ssid  = "cdac";       const char* password  = "";
+
+RTC_DATA_ATTR char* ssid  = "cdac";   RTC_DATA_ATTR char* password = "";
+
+
+#endif

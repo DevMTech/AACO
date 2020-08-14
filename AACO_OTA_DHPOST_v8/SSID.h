7963819f1a298ce453b23046642ce3c80092f22b
debajyotim <debajyotim@cdac.in>
2020-08-14 11:04:51 +0530

    OTA BIN (NON-STATIC IP)

--- AACO_OTA_DHPOST_v8/SSID.h
+++ AACO_OTA_DHPOST_v8/SSID.h
@@ -5,11 +5,11 @@
 //const char* ssid  = "";       const char* password  = "";
 RTC_DATA_ATTR char* ssid  = "";   RTC_DATA_ATTR char* password = "";
 
-IPAddress local_IP(, , , ); // Set your Static IP address
-IPAddress gateway(, , , ); // Set your Gateway IP address
-IPAddress subnet(, , , );
-IPAddress primaryDNS(, , , );   //optional
-IPAddress secondaryDNS(, , , ); //optional
+//IPAddress local_IP(, , , ); // Set your Static IP address
+//IPAddress gateway(, , , ); // Set your Gateway IP address
+//IPAddress subnet(, , , );
+//IPAddress primaryDNS(, , , );   //optional
+//IPAddress secondaryDNS(, , , ); //optional
 
 
 #endif

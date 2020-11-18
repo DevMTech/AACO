bf4a5fcffee6b3f13234c338c01bc80fcf780671
debajyotim <debajyotim@cdac.in>
2020-11-18 17:48:39 +0530

    Update SSID.h

--- AACO_OTA_BME680_TSL2561_CCS811_DUST_DHPOST_v9/SSID.h
+++ AACO_OTA_BME680_TSL2561_CCS811_DUST_DHPOST_v9/SSID.h
@@ -3,8 +3,8 @@
 
 //const char* ssid  = "cdac";       const char* password  = "";
 
-//RTC_DATA_ATTR char* ssid  = "cdac";   RTC_DATA_ATTR char* password = "";
+RTC_DATA_ATTR char* ssid  = "cdac";   RTC_DATA_ATTR char* password = "";
 //RTC_DATA_ATTR char* ssid  = "ASUS_X00TD";   RTC_DATA_ATTR char* password = "";
-RTC_DATA_ATTR char* ssid  = "NeMo";   RTC_DATA_ATTR char* password = "19stm3289";
+//RTC_DATA_ATTR char* ssid  = "";   RTC_DATA_ATTR char* password = "";
 
 #endif
